0838c56b652c850bfd3fafede25128e9b95c1f90
no_author <contact@osivia.com>
2017-01-06 13:36:44 +0000

    Fixes Drafts count when ES is not enabled

--- opentoutatice-checkin/src/main/java/fr/toutatice/ecm/checkin/portal/infos/provider/CheckinFolderishInfosProvider.java
+++ opentoutatice-checkin/src/main/java/fr/toutatice/ecm/checkin/portal/infos/provider/CheckinFolderishInfosProvider.java
@@ -9,6 +9,9 @@ import java.util.Map;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IterableQueryResult;
+import org.nuxeo.ecm.core.query.sql.NXQL;
+import org.nuxeo.elasticsearch.ElasticSearchConstants;
 import org.nuxeo.elasticsearch.api.ElasticSearchService;
 import org.nuxeo.elasticsearch.api.EsResult;
 import org.nuxeo.elasticsearch.query.NxQueryBuilder;
@@ -27,16 +30,20 @@ import fr.toutatice.ecm.platform.core.services.infos.provider.DocumentInformatio
  */
 public class CheckinFolderishInfosProvider implements DocumentInformationsProvider {
 
+    /** ElasticSearch's server enabled indicator. */
+    private static boolean isEsEnabled;
+
     /** ElasticSearch service. */
     private final ElasticSearchService esService;
-
 
     /**
      * Constructor.
      */
     public CheckinFolderishInfosProvider() {
         super();
-
+        // Is ElasticSearch activated
+        isEsEnabled = Framework.isBooleanPropertyTrue(ElasticSearchConstants.ES_ENABLED_PROPERTY);
+        
         // ElasticSearch service
         this.esService = Framework.getService(ElasticSearchService.class);
     }
@@ -45,14 +52,13 @@ public class CheckinFolderishInfosProvider implements DocumentInformationsProvid
     /**
      * {@inheritDoc}
      */
+    // FIXME: modify fetchInfos to take ES into account (new param or abstract class)
     @Override
     public Map<String, Object> fetchInfos(CoreSession coreSession, DocumentModel currentDocument) throws ClientException {
         // Infos
         Map<String, Object> infos = new HashMap<String, Object>(1);
 
         if (currentDocument.isFolder()) {
-            // Query builder
-            NxQueryBuilder queryBuilder = new NxQueryBuilder(coreSession);
             // Draft folder path
             String userWsPath = DocumentCheckinHelper.getInstance().getDraftsFolderPath(coreSession, currentDocument);
             // Checkout parent identifier
@@ -64,16 +70,30 @@ public class CheckinFolderishInfosProvider implements DocumentInformationsProvid
             query.append("AND ecm:mixinType = 'OttcDraft' ");
             query.append("AND ottcDft:checkoutParentId = '").append(checkoutParentId).append("' ");
             query.append("AND ecm:currentLifeCycleState <> 'deleted' ");
-            queryBuilder.nxql(query.toString());
 
-            // ElasticSearch result
-            EsResult draftsIds = this.esService.queryAndAggregate(queryBuilder);
+            long draftCount = 0;
+            if (isEsEnabled) {
+                // ES query
+                NxQueryBuilder queryBuilder = new NxQueryBuilder(coreSession).nxql(query.toString());
 
-            if ((draftsIds != null) && (draftsIds.getRows() != null)) {
-                // Draft count
-                long draftCount = draftsIds.getRows().size();
-                infos.put("draftCount", draftCount);
+                EsResult draftsIds = this.esService.queryAndAggregate(queryBuilder);
+
+                if ((draftsIds != null) && (draftsIds.getRows() != null)) {
+                    // Draft count
+                    draftCount = draftsIds.getRows().size();
+                }
+            } else {
+                // VCS query
+                IterableQueryResult draftsRows = coreSession.queryAndFetch(query.toString(), NXQL.NXQL, new Object[0]);
+
+                if (draftsRows != null) {
+                    // Draft count
+                    draftCount = draftsRows.size();
+                }
             }
+
+            // Store infos
+            infos.put("draftCount", draftCount);
         }
 
         return infos;
